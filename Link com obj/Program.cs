ed5df6d95569c30d51f860116c5f57486a15f3cb
Anderson Vargas <andersomrobertomachadodevargas@gmail.com>
2024-01-29 14:43:52 -0300

    acrescentado firstordafault numa nova expresssao lambda

--- Link com obj/Program.cs
+++ Link com obj/Program.cs
@@ -8,6 +8,7 @@ class Progran
         List<Worker> workers = new List<Worker>() {
 
             new Worker("Jose", 4000),
+            new Worker("Josias", 5000),
             new Worker("Elias", 9000),
             new Worker("Pedro", 5300),
         
@@ -17,6 +18,7 @@ class Progran
 
         var g = workers.Where(w=> w.salario<6000.00).Select(w => w.salario * 2);
         var d = workers.Where(w => w.nome[0] == 'P').Select(o=> o.nome);
+        var w = workers.FirstOrDefault(obj => obj.nome[0] == 'J');
         foreach ( var a in g)
         {
             Console.WriteLine(a);
@@ -25,6 +27,6 @@ class Progran
         {
             Console.WriteLine(a);
         }
-
+        Console.WriteLine(w);
     }
 }
